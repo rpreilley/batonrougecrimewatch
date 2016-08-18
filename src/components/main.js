fd52d4ef661099a487f70a34bcf6472aae5deec1
Robert Reilley <rpreilley@gmail.com>
2016-08-18 11:55:36 -0500

    added import

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './search';
 import RenderMap from './map';
+import fetch from 'whatwg-fetch';
 
 class Main extends React.Component {
   constructor(props){
